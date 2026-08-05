11fb80824a90741f798cb165ecc5f9406f19c204
Andiweli <stuermer.andreas@gmail.com>
2026-08-05 11:13:57 +0200

    chore: add ioBroker Prettier configuration [skip ci]

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+// ioBroker Prettier configuration
+import prettierConfig from "@iobroker/eslint-config/prettier.config.mjs";
+
+export default {
+    ...prettierConfig,
+    singleQuote: false,
+};
